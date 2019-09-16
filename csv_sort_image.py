272c3e487a670f1e97cd8f96847eda2204b9dd4b
yinglanh <yinglanh@student.unimelb.edu.au>
2019-09-17 00:30:08 +1000

    split data to train val

--- csv_sort_image.py
+++ csv_sort_image.py
@@ -1,6 +1,7 @@
 import csv
 import shutil
 import os
+import split_folders
 
 def sort_images():
     target_path = 'data/image_sort'
@@ -18,5 +19,16 @@ def sort_images():
                 os.makedirs(target_path + '/' + row[1])
                 shutil.copy(original_path + '/' + row[0], target_path + '/' + row[1])
 
+
+def split_to_train_test():
+    # Split val/test with a fixed number of items e.g. 100 for each set.
+    # To only split into training and validation set, use a single number to `fixed`, i.e., `10`.
+    split_folders.ratio('data/image_sort', output="data/image_train", seed=1337, ratio=(.8, .2))  # default values
+
+#$split_folders data/image_sort --ratio .8 .2
+
+
+
 if __name__ == "__main__":
-    sort_images()
+    #sort_images()
+    split_to_train_test()
